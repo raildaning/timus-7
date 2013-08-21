b54e84e26b5298b3d6b58c5177a8c8c294310d9f
ImMrSn <misha.sn@list.ru>
2013-08-21 04:45:56 +0400

    Create 1877.cpp

---
+++ 1877.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    int a,b;
+    cin >> a >> b;
+    if (a%2==0 || b%2==1)
+        cout << "yes"; else cout << "no";
+    return 0;
+}
+//1877
+//Accepted (0.031) 192КБ
